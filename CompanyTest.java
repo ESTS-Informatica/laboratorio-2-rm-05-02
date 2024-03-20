d70a78af2a7307d4220912dd752128ddac960066
201902549 <201902549@estudantes.ips.pt>
2024-03-20 10:29:03 +0000

    Nivel 3
    Feito ate Nivel 3, Part 3
    Faltou a Implemente no Company

---
+++ CompanyTest.java
@@ -0,0 +1,64 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A classe de teste CompanyTest.
+ *
+ * @author  (seu nome)
+ * @version (um número da versão ou uma data)
+ */
+public class CompanyTest
+{
+    private User client1;
+    private User client2;
+    private User seller1;
+    private User seller2;
+    private Property property1;
+    private Property property2;
+    private Company company;
+    
+    
+
+
+
+    /**
+     * Define a 'fixture' do teste.
+     *
+     * Chamado antes de cada método de caso de teste.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        company = new Company();
+        client1 = new User("José Manuel", "911111111", "zemanel@yahoo.com");
+        client2 = new User("António Francisco", "922222222", "tochico@hotmail.com");
+        seller1 = new User("Fernando Fernandes", "966777101", "fefe@remax.pt");
+        seller2 = new User("Rodrigo Rodrigues", "966777152", "roro@remax.pt");
+    }
+    
+    /**
+     * Construtor default para a classe de teste CompanyTest
+     */
+    @Test
+    public void testCompany() {
+        assertNotNull(company.getClients());
+        assertNotNull(company.getSellers());
+        assertNotNull(company.getProperties());
+        assertNotNull(company.getSells());
+    }
+
+    /**
+     * Desfaz a 'fixture' do teste.
+     *
+     * Chamado após cada método de teste de caso.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+        
+    }
+}
